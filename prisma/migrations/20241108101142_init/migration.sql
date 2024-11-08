2a9a5e17d9b499803e9ce7e61bd0c1a9980ef0d5
mimi-uxui-dev <uxui.mimi@gmail.com>
2024-11-08 11:19:15 +0100

    setup prisma db

---
+++ prisma/migrations/20241108101142_init/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "Application2" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "userId" INTEGER NOT NULL,
+    "university" TEXT NOT NULL,
+    "fileUrl" TEXT NOT NULL
+);
