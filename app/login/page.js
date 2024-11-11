6cef3a901ca0660effef145c8fcec40e2f4bf9c6
mimi-uxui-dev <uxui.mimi@gmail.com>
2024-11-11 20:30:25 +0100

    toaster

--- app/login/page.js
+++ app/login/page.js
@@ -4,7 +4,6 @@ import { useRouter } from "next/navigation";
 import { useState } from "react";
 
 export default function Register() {
-  const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [loading, setLoading] = useState(false);
