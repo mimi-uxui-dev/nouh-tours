0921ae02fe2c3bd2d92e43cdb512490fc2f0ec97
mimi-uxui-dev <uxui.mimi@gmail.com>
2024-11-14 15:46:47 +0100

    sessions to nav

--- components/Nav.js
+++ components/Nav.js
@@ -1,13 +1,27 @@
+import { useSession, signOut } from "next-auth/react";
 import Link from "next/link";
 import React from "react";
 
 function Nav() {
+  const { data, status } = useSession();
+  // console.log("data -> ", data, " Status -> ", status);
+
   return (
     <nav>
       <Link href={"/"}>Home</Link>
       <Link href={"/"}>Services</Link>
-      <Link href={"/login"}>Login</Link>
-      <Link href={"/register"}>Register</Link>
+
+      {status === "authenticated" ? (
+        <>
+          <Link href={"/dashboard/user"}>Dashboard</Link>
+          <a onClick={() => signOut({ callbackUrl: "/login" })}>Logout</a>
+        </>
+      ) : (
+        <>
+          <Link href={"/login"}>Login</Link>
+          <Link href={"/register"}>Register</Link>
+        </>
+      )}
     </nav>
   );
 }
