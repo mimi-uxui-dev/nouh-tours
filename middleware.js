1db7927797ad901963d8ecb0476e6e36f6669d6d
mimi-uxui-dev <uxui.mimi@gmail.com>
2024-11-14 19:55:24 +0100

    Admin Role based authorization + api

--- middleware.js
+++ middleware.js
@@ -1,3 +1,34 @@
-export { default } from "next-auth/middleware";
+import { withAuth } from "next-auth/middleware";
+import { NextResponse } from "next/server";
+// export { default } from "next-auth/middleware";
 
-export const config = { matcher: ["/dashboard/:path*"] };
+export const config = {
+  matcher: [
+    "/dashboard/user/:path*",
+    "/dashboard/admin/:path*",
+    "/api/user/:path*",
+    "/api/admin/:path*",
+  ],
+};
+
+// Role based authorization
+
+export default withAuth(
+  async function middleware(req) {
+    const url = req.nextUrl.pathname;
+    const userRole = req?.nextauth?.token?.user?.role;
+
+    if (url?.includes("/admin") && userRole !== "admin") {
+      return NextResponse.redirect(new URL("/", req.url));
+    }
+  },
+  {
+    callbacks: {
+      authorized: ({ token }) => {
+        if (!token) {
+          return false;
+        }
+      },
+    },
+  }
+);
