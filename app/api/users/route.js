4b973963d90858f7133b116889b88fa9d094d30b
mimi-uxui-dev <uxui.mimi@gmail.com>
2024-11-17 13:23:48 +0100

    API GET users

--- app/api/users/route.js
+++ app/api/users/route.js
@@ -6,7 +6,8 @@ export async function GET() {
   await dbConnect();
 
   try {
-    const users = await User.find().populate("universitiesAppliedTo");
+    // const users = await User.find().populate("universitiesAppliedTo");
+    const users = await User.find();
     return NextResponse.json(users, { status: 200 });
   } catch (error) {
     return NextResponse.json(
