2dbb23d7b37aec493bd26f961d0cc1c42e57f4bc
mimi-uxui-dev <uxui.mimi@gmail.com>
2024-12-03 11:23:39 +0100

    nav style

--- components/Nav.js
+++ components/Nav.js
@@ -1,33 +1,51 @@
 import { useSession, signOut } from "next-auth/react";
+import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import logo from "@/app/images/logo.png";
 
 function Nav() {
   const { data, status } = useSession();
   // console.log("data -> ", data, " Status -> ", status);
 
   return (
-    <nav className="flex justify-between">
-      <Link href={"/"}>Home</Link>
-      <Link href={"/"}>Services</Link>
+    <div className="flex justify-center items-center h-screen bg-gray-100 ">
+      <nav className="text-white max-w-screen-2xl nav flex justify-between items-center w-full  lg:px-0 px-5">
+        <Link href={"/"}>
+          <Image
+            src={logo}
+            alt="nouh tours logo"
+            className="h-10 w-full lg:h-20"
+          />
+        </Link>
 
-      {status === "authenticated" ? (
-        <>
-          <Link
-            href={`/dashboard/${
-              data?.user?.role === "admin" ? "admin" : "user"
-            }`}
-          >
-            Dashboard {data?.user?.fullName} ({data?.user?.role})
-          </Link>
-          <a onClick={() => signOut({ callbackUrl: "/login" })}>Logout</a>
-        </>
-      ) : (
-        <>
-          <Link href={"/login"}>Login</Link>
-        </>
-      )}
-    </nav>
+        <div className="flex space-x-10 items-center ">
+          <div className="lg:flex space-x-10 items-center hidden">
+            <Link href={"/"}>Home</Link>
+            <Link href={"/"}>Our Services</Link>
+            <Link href={"/"}>Our Process</Link>
+            <Link href={"/"}>Contact</Link>
+            <Link href={"/"}>About Us</Link>
+          </div>
+          {status === "authenticated" ? (
+            <>
+              <Link
+                href={`/dashboard/${
+                  data?.user?.role === "admin" ? "admin" : "user"
+                }`}
+              >
+                Dashboard
+              </Link>
+              <a onClick={() => signOut({ callbackUrl: "/login" })}>Logout</a>
+            </>
+          ) : (
+            <Link className="btn" href={"/login"}>
+              Login
+            </Link>
+          )}
+        </div>
+      </nav>
+    </div>
   );
 }
 
---
+++ components/sections/Hero.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Hero() {
+  return <div className="hero-section"></div>;
+}
+
+export default Hero;
