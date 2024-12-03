cd42e5931eeeaebc6c8a0c8939c1ecbf84fd4bc0
mimi-uxui-dev <uxui.mimi@gmail.com>
2024-12-03 21:33:44 +0100

    stats

--- components/sections/Stats.js
+++ components/sections/Stats.js
@@ -1,7 +1,24 @@
 import React from "react";
 
 function Stats() {
-  return <div></div>;
+  const stats = [
+    {
+      id: 1,
+      number: "20K",
+      text: "Business trust Syncro",
+    },
+    {
+      id: 2,
+      number: "98%",
+      text: "Business trust Syncro",
+    },
+    {
+      id: 3,
+      number: "150k",
+      text: "Business trust Syncro",
+    },
+  ];
+  return <article></article>;
 }
 
 export default Stats;
