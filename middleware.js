6625e3bdd5f72fde1132297aa98cef4fd1359260
mimi-uxui-dev <uxui.mimi@gmail.com>
2024-12-22 21:00:00 +0100

    cors

--- middleware.js
+++ middleware.js
@@ -18,6 +18,11 @@ export default withAuth(
     const url = req.nextUrl.pathname;
     const userRole = req?.nextauth?.token?.user?.role;
 
+    // cors
+    if (url?.includes("/api")) {
+      NextResponse.next().headers.append("Access-Control-Allow-Origin", "*");
+    }
+
     if (url?.includes("/admin") && userRole !== "admin") {
       return NextResponse.redirect(new URL("/", req.url));
     }
