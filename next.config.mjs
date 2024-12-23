0b5a979afddb0ad198117e24e5708e507e91a616
mimi-uxui-dev <uxui.mimi@gmail.com>
2024-12-23 07:04:37 +0100

    updates

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ import config from "./config.js";
 const { DB_URI, API, NEXTAUTH_SECRET } = config; // Destructure to get DB_URI
 
 const nextConfig = {
+  output: "export",
   env: {
     DB_URI,
     API,
