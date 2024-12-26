818505e6b3f0052822065ca8245bc2d1d0f07608
mimi-uxui-dev <uxui.mimi@gmail.com>
2024-12-26 21:57:57 +0100

    added env to next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,13 +2,13 @@
 
 // const { DB_URI, API, NEXTAUTH_SECRET } = config;
 
-const nextConfig = {};
-// const nextConfig = {
-//   env: {
-//     DB_URI,
-//     API,
-//     NEXTAUTH_SECRET,
-//   },
-// };
+// const nextConfig = {};
+const nextConfig = {
+  env: {
+    DB_URI: process.env.DB_URI,
+    API: process.env.API,
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+  },
+};
 
 export default nextConfig;
