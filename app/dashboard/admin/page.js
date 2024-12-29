65219d50c25dfc7d7075ece0d50cee06bfa664c2
mimi-uxui-dev <uxui.mimi@gmail.com>
2024-12-29 13:01:59 +0100

    /

--- app/dashboard/admin/page.js
+++ app/dashboard/admin/page.js
@@ -12,7 +12,7 @@ export default function AdminDashboard() {
   async function fetchData() {
     setLoading(true);
     try {
-      const res = await fetch(`${process.env.API}/users/`, {
+      const res = await fetch(`${process.env.API}/users`, {
         method: "GET",
         redirect: "follow", // Allows following the redirect
       });
--- app/dashboard/user/page.js
+++ app/dashboard/user/page.js
@@ -15,7 +15,7 @@ export default function UserDashboard() {
   async function fetchData() {
     setLoading(true);
     try {
-      const res = await fetch(`${process.env.API}/users/`, {
+      const res = await fetch(`${process.env.API}/users`, {
         method: "GET",
         redirect: "follow", // Allows following the redirect
       });
