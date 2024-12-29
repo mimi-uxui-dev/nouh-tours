4705b23134accfce434d533956c3f24083a1d367
mimi-uxui-dev <uxui.mimi@gmail.com>
2024-12-29 13:32:06 +0100

    c

--- middleware.js
+++ middleware.js
@@ -26,16 +26,16 @@ export default withAuth(
       );
     }
 
-    // res.headers.append("Access-Control-Allow-Credentials", "true");
-    // res.headers.append("Access-Control-Allow-Origin", "*");
-    // res.headers.append(
-    //   "Access-Control-Allow-Methods",
-    //   "GET,DELETE,PATCH,POST,PUT"
-    // );
-    // res.headers.append(
-    //   "Access-Control-Allow-Headers",
-    //   "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
-    // );
+    res.headers.append("Access-Control-Allow-Credentials", "true");
+    res.headers.append("Access-Control-Allow-Origin", "*");
+    res.headers.append(
+      "Access-Control-Allow-Methods",
+      "GET,DELETE,PATCH,POST,PUT"
+    );
+    res.headers.append(
+      "Access-Control-Allow-Headers",
+      "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+    );
 
     if (url?.includes("/admin") && userRole !== "admin") {
       return NextResponse.redirect(new URL("/", req.url));
