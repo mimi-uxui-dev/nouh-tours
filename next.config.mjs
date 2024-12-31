1c5665e819a22c4d4dec559fabc111d52e9b855f
mimi-uxui-dev <uxui.mimi@gmail.com>
2024-12-31 14:53:28 +0100

    w

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,21 @@ const nextConfig = {
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
     SECRET: process.env.SECRET,
   },
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "nouhtours.com", // Match non-www domain
+          },
+        ],
+        destination: "https://www.nouhtours.com/:path*",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
