a35c0c76f8788a1d36e0fb51484dd9619f0b4063
mimi-uxui-dev <uxui.mimi@gmail.com>
2024-12-31 19:51:22 +0100

    xxx

--- next.config.mjs
+++ next.config.mjs
@@ -20,38 +20,31 @@ const nextConfig = {
       },
     ];
   },
-  async rewrites() {
+
+  async headers() {
     return [
       {
+        // matching all API routes
         source: "/api/:path*",
-        destination: "https://nouhtours.com/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "https://www.nouhtours.com",
+          }, // replace this your actual origin
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,DELETE,PATCH,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
       },
     ];
   },
-  // async headers() {
-  //   return [
-  //     {
-  //       // matching all API routes
-  //       source: "/api/:path*",
-  //       headers: [
-  //         { key: "Access-Control-Allow-Credentials", value: "true" },
-  //         {
-  //           key: "Access-Control-Allow-Origin",
-  //           value: "https://www.nouhtours.com",
-  //         }, // replace this your actual origin
-  //         {
-  //           key: "Access-Control-Allow-Methods",
-  //           value: "GET,DELETE,PATCH,POST,PUT",
-  //         },
-  //         {
-  //           key: "Access-Control-Allow-Headers",
-  //           value:
-  //             "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-  //         },
-  //       ],
-  //     },
-  //   ];
-  // },
 };
 
 export default nextConfig;
