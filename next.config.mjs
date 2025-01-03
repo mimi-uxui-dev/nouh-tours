e10d278b406b227014aa4d0044bef4ac8cfc29d5
mimi-uxui-dev <uxui.mimi@gmail.com>
2025-01-03 20:17:55 +0100

    cc

--- next.config.mjs
+++ next.config.mjs
@@ -12,7 +12,7 @@ const nextConfig = {
         has: [
           {
             type: "host",
-            value: "https://nouhtours.com", // Match non-www domain
+            value: "https://www.nouhtours.com", // Match non-www domain
           },
         ],
         destination: "https://www.nouhtours.com/:path*",
